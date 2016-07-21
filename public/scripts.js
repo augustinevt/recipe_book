b9438f72bd6d070511ec3ca00c1e4bcc20b99800
August von Trapp and Sebastian Prokuski <august+sebastian@>
2016-07-21 15:44:36 -0700

    add ants

--- public/scripts.js
+++ public/scripts.js
@@ -8,4 +8,19 @@ $(function() {
   submitTags = function() {
     document.getElementById("recipe_form").submit();
   };
+
+
+  $('#search-form').submit(function(e) {
+    if ( $('#search').val() === "ants" ) {
+      e.preventDefault()
+      $('.antz').show();
+      console.log('yes ants')
+
+    }
+    console.log('no ants')
+  });
+
+  antShow = function() {
+    $('antz').show();
+  };
 });
